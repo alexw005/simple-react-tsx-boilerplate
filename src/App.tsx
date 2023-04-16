4d4eff878f56c52c84f03ab58ba3789a2a31b30d
alexhandsome2 <alexhandsome_guy2@yahoo.com>
2023-04-16 15:57:51 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Privacy Policy</h1>
+      </header>
+      <div className='Text'>
+      <p>We do not collect any personal data from our users. We respect your privacy and are committed to protecting it. You can use our website without providing any personal information.
+</p><p>We may collect non-personal information such as your IP address, browser type, operating system, and other technical information to improve the performance and security of our website. This information is not linked to any personal information and is used solely for analytical purposes.
+</p><p>We do not use cookies or any other tracking technologies on our website.
+</p><p>If you have any questions or concerns about our privacy policy, please contact us at qposcorp@gmail.com.</p>
+</div>
+    </div>
+  );
+}
+
+export default App;
